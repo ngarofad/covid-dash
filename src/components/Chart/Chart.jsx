aa322ebd0f3de2f3e49d95b57edcf80c1abe79a1
ngarofad <annasrofai@gmail.com>
2021-05-01 22:54:43 +0700

    push lagi

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import {Line, Bar} from 'react-chartjs-2';
 
-const Chart = () => {
+import styles from './Chart.module.css'
+
+
+
+const Chart = ({data: {confirmed, deaths, recovered}, country}) => {
+    const [dailyData, setDailyData] = useState([]);
+    
+    useEffect(() => {
+        const fetchAPI = async () => {
+            // //const dailyData = await fetchDailyData();
+            setDailyData(await fetchDailyData());
+        } 
+        
+        fetchAPI();
+    },[]);
+
+
+    const lineChart = (
+        dailyData.length 
+        ? (
+        <Line
+            data={{
+                labels: dailyData.map(({ date }) => date),
+                datasets: [{
+                    data: dailyData.map(({ confirmed }) => confirmed),
+                    label: 'infected',
+                    borderColor: '#3333FF',
+                    fill: true,
+                },{
+                    data: dailyData.map(({ deaths }) => deaths),
+                    label: 'deaths',
+                    borderColor: '#red',
+                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                    fill: true,
+
+                }],
+            }}
+        /> 
+        ) : null   
+    );
+
+    const barChart =(
+        confirmed ? (
+            <Bar
+            data= {{
+                labels: ['Infected', 'Recovered', 'Deaths'],
+                datasets: [
+                    {
+                    label:'title',
+                    backgroundColor:[
+                        'rgba(0, 0, 255, 0.5)',
+                        'rgba(0, 255, 0, 0.5)',
+                        'rgba(255, 0, 0, 0.5)'],
+                        data: [confirmed.value, recovered.value, deaths.value],
+                },
+            ],
+
+            }}
+            options={
+                {
+                    legend: {display: false},
+                    tittle: {display:true, text:`Current State in ${country}` }
+                }
+            }
+            />
+        ) : null
+    );
+    
+
     return (
-        <h1>Chart bos</h1>
-    )
+        <div className={styles.container}>
+            
+            {country? barChart : lineChart}
+            
+        </div>
+    )    
 }
 
 export default Chart;
